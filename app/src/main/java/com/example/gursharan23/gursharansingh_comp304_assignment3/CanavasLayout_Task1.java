e674122007df467e81ee2bf390f961ecc06a8580
gursharan23 <iamgursharan@gmail.com>
2018-03-01 23:58:11 -0500

    Added the onItemSelected for spinner

--- app/src/main/java/com/example/gursharan23/gursharansingh_comp304_assignment3/CanavasLayout_Task1.java
+++ app/src/main/java/com/example/gursharan23/gursharansingh_comp304_assignment3/CanavasLayout_Task1.java
@@ -3,7 +3,7 @@ package com.example.gursharan23.gursharansingh_comp304_assignment3;
 * Date : February 28, 2018
 * Author : Gursharan Singh
 * Description : This class handles all the events of the canvas layout activity
-* Version : 0.1 - Created the canvas layout
+* Version : 0.2 - Added the onItemSelected for spinner
 * GitHub Info: https://github.com/iamgursharan
  */
 
@@ -14,12 +14,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class CanavasLayout_Task1 extends Activity {
+public class CanavasLayout_Task1 extends Activity implements AdapterView.OnItemSelectedListener {
 
     //Declaring  canvas objects
     Paint paint;
@@ -31,6 +32,8 @@ public class CanavasLayout_Task1 extends Activity {
     int startY=50;
     int endX=50;
     int endY=50;
+    // Declaring the thickness variable
+    int thickness;
 
 
     @Override
@@ -41,7 +44,7 @@ public class CanavasLayout_Task1 extends Activity {
         // Instantiating canvas objects
         paint=new Paint();
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(40);
+      //  paint.setStrokeWidth(thickness);
         bitmap=Bitmap.createBitmap((int)getWindowManager().getDefaultDisplay().getWidth(),
                 (int)getWindowManager().getDefaultDisplay().getHeight(),Bitmap.Config.ARGB_8888);
         canvas=new Canvas(bitmap);
@@ -51,15 +54,18 @@ public class CanavasLayout_Task1 extends Activity {
         canvas.drawColor(Color.BLACK);
         // Declaring spinner
         Spinner spinner=findViewById(R.id.thicknessSpinner);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.thickness_array,
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
+                R.array.thickness_array,
                 android.R.layout.simple_spinner_item);
         // Specify layout
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Applying adapter
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
     }
 
+    // This method draws line on the canvas. It accepts one paramenter- canvas(Canvas)
     public void drawLine(Canvas canvas)
     {
         canvas.drawLine(startX,startY,endX,endY,paint);
@@ -67,6 +73,8 @@ public class CanavasLayout_Task1 extends Activity {
         startY=endY;
     }
 
+    //This method handles the click events of all the arrow buttons. It accepts one parameter -
+    // view (View)
     public void Click(View view)
     {
 
@@ -74,7 +82,6 @@ public class CanavasLayout_Task1 extends Activity {
             case R.id.leftArrowBtn:
                 endX=endX-5;
                 drawLine(canvas);
-                Toast.makeText(this,"Left arrow clicked",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.rightArrowBtn:
                 endX=endX+5;
@@ -91,5 +98,33 @@ public class CanavasLayout_Task1 extends Activity {
         }
     }
 
+    // This is the onItemSelected that triggers for every item selected in a spinner.
+    // It sets strokewidth of the paint object
+    public void onItemSelected(AdapterView<?> parent,View view, int pos, long id)
+    {
+        switch((String)parent.getItemAtPosition(pos))
+        {
+            case "20": thickness=20;
+                Toast.makeText(this,"20 selected",Toast.LENGTH_SHORT).show();
+                break;
+            case "40": thickness=40;
+                Toast.makeText(this,"40 selected",Toast.LENGTH_SHORT).show();
+                break;
+            case "60": thickness=60;
+                Toast.makeText(this,"60 selected",Toast.LENGTH_SHORT).show();
+                break;
+            case "80": thickness=80;
+                Toast.makeText(this,"80 selected",Toast.LENGTH_SHORT).show();
+                break;
+            case "100": thickness=100;
+                Toast.makeText(this,"100 selected",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        paint.setStrokeWidth(thickness);
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
